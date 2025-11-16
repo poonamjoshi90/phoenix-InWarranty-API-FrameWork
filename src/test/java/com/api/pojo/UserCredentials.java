dfd1464ccf3b9483f51d93c631baf81b2c057991
Poonam <automationpoonam19@gmail.com>
2025-11-16 13:06:30 +0530

    basecode

--- src/test/java/com/api/pojo/UserCredentials.java
+++ src/test/java/com/api/pojo/UserCredentials.java
@@ -1,36 +1,10 @@
 package com.api.pojo;
 
-public class UserCredentials {
+public record UserCredentials 
+(
 	
-	private String username;
-	private String password;
+	 String username,
+	 String password
+	 )
 	
-	public UserCredentials(String username, String password) {
-		super();
-		this.username = username;
-		this.password = password;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	@Override
-	public String toString() {
-		return "UserCredentials [username=" + username + ", password=" + password + "]";
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-
-}
+	{}
--- src/test/java/com/api/test/CreateJobAPITest.java
+++ src/test/java/com/api/test/CreateJobAPITest.java
@@ -2,6 +2,9 @@ package com.api.test;
 
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 import com.api.constant.Roles;
@@ -30,12 +33,12 @@ public class CreateJobAPITest {
 	{
 		Customer customer =new Customer("Poonam", "Joshi", "9099091290", "", "test@mail.com", "");
 		CustomerAddress customeraddress = new CustomerAddress("Flat-No-967", "ABC","Test123", "Test12", "TestArea12", "92122","India", "Delhi");
-		CustomerProduct customer_product = new CustomerProduct("2025-10-07T18:30:00.000Z", "12429730786260", "12429730785260", "124297307856270", "2025-10-07T18:30:00.000Z", 1, 1);
+		CustomerProduct customer_product = new CustomerProduct("2025-10-07T18:30:00.000Z", "40129730786260", "40129730786260", "40129730786260", "2025-10-07T18:30:00.000Z", 1, 1);
 		Problems problems = new Problems(1,"Batary Issue");
-		Problems[] problemArray= new  Problems[1] ;
-		problemArray[0]=problems;
+		List<Problems> problemList= new ArrayList<Problems>();
+		problemList.add(problems);
 		
-		CreateJobPayload createJobPayload= new CreateJobPayload(0, 2, 1, 1, customer, customeraddress, customer_product, problemArray);
+		CreateJobPayload createJobPayload= new CreateJobPayload(0, 2, 1, 1, customer, customeraddress, customer_product, problemList);
 		
 		given()
 		.spec(SpecUtil.RequestSpecWIthAuth(Roles.FD, createJobPayload))
@@ -44,7 +47,7 @@ public class CreateJobAPITest {
 		.then()
 		.spec(SpecUtil.responsespec_Ok())
 		.and()
-	      .body("message",equalTo("Job created successfully."))
+	      .body("message",equalTo("Job created successfully. "))
 	      .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("responseSchema/CreateJobAPIResponseSchema.json"));
 	     
 		
